04c82362d54b217703a473a28a6626dcae632843
n-white <neil.r.whitehead@gmail.com>
2016-08-04 17:30:49 -0700

    server.js file complete

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+require('./passport.js');
+
+module.exports = io;
+
+// Invoke middleware function on app to 'use' all the middleware functions
+const middleware = require('./helpers/middleware.js');
+middleware(app);
+
+// Invoke routers function on app to provide access to all routes defined
+const routers = require('./helpers/routes.js');
+io.on('connection', (socket) => routers(socket, io, app));
+
+// App now listening on port 3000
+server.listen(3000, (err) => {
+	err ? console.log('server error', err) : console.log('server listening on 3000')
+})
