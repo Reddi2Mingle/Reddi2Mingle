5037913efd56633d3779df8b89c32f3f170ce8d6
Jennifer Ong <jenn.r.ong@gmail.com>
2016-08-05 19:23:23 -0700

    Add front-end routing and file skeleton

---
+++ client/app/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Link } from 'react-router';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Reddi2Mingle</h1>
+        <ul role="nav">
+          <li><Link to="/" onlyActiveOnIndex>Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">SignUp</Link></li>
+          <li><Link to="/matchmaker">Matchmaker</Link></li>
+          <li><Link to="/matched">Matched</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    );
+  }
+}
---
+++ client/app/auth/Signup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Signup (props) => {
+  <div> 
+    <h1> Sign-up Here </h1> 
+  </div>
+}
