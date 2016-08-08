5b06bc90036c8be018e39f34e393adc453e97d4c
Jennifer Ong <jenn.r.ong@gmail.com>
2016-08-08 12:27:19 -0700

    Add socket emit upon button click

--- client/app/auth/Signup.jsx
+++ client/app/auth/Signup.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import socket from 'socket.io-client';
 
 export default class Signup extends React.Component {
+  handleClick() {
+    socket.emit('userSignUp');
+  }
+
   render() {
     return (
       <div> 
         <h1> Sign-up Here </h1> 
+        <button onClick={this.handleClick}> Click here </button>
       </div>
     );
   }
