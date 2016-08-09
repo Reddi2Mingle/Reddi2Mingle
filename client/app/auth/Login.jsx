da481251604464d58c43e23cb1ef4547faed5992
Tyler Becks <tylerdbecks@gmail.com>
2016-08-08 20:12:34 -0700

    Remove socket from Login

--- client/app/auth/Login.jsx
+++ client/app/auth/Login.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
-
-export default class Login extends React.Component {
-  handleClick() {
-    socket.emit('userLogin');
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Login with your Reddit Account</h1>
-        <button onClick={this.handleClick}>Login with Reddit</button>
-      </div>
-    );
-  }
-}
+export default () => (
+  <div>
+    <h1>Login with your Reddit Account</h1>
+    <button onClick={this.handleClick}>Login with Reddit</button>
+  </div>
+);
