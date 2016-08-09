5526c73ebd1a4737528e1f8b6f5b2053772fa0af
Christine Chou <christinejchou@gmail.com>
2016-08-08 20:19:00 -0700

    Update docker to require node & dependencies

--- docker/init.sql
+++
@@ -1,7 +0,0 @@
--- Any SQL commands in this file will be
--- run when the database initializes.
-
--- Example of how SQL commands would look:
--- CREATE USER reddi;
--- CREATE DATABASE reddi;
--- GRANT ALL PRIVILEGES ON DATABASE ether TO reddi;
