700cda8de4efc0f624cf541ae0c91add1af02ac7
Tyler Becks <tylerdbecks@gmail.com>
2016-08-12 10:52:37 -0700

    Add Reddit key and secret as process.env

--- server/passport.js
+++ server/passport.js
@@ -29,8 +29,8 @@ passport.deserializeUser((obj, done) => {
 //   credentials (in this case, an accessToken, refreshToken, and Reddit
 //   profile), and invoke a callback with a user object.
 passport.use(new RedditStrategy({
-  clientID: 'T3zDXS9GxKukbA',
-  clientSecret: 'TAKMSJzrlZPzTWxK5O3w7OglWA8',
+  clientID: REDDIT_CONSUMER_KEY,
+  clientSecret: REDDIT_CONSUMER_SECRET,
   callbackURL: 'http://127.0.0.1:3000/auth/reddit/callback',
 },
   (accessToken, refreshToken, profile, done) => {
