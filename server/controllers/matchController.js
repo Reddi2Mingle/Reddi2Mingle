f8b9a9654edadad8ffb489e9fa73366da802b473
n-white <neil.r.whitehead@gmail.com>
2016-08-12 11:31:26 -0700

    changes to naming conventions in seed.js file and adding functionality for querying user profile data (in progress)

--- server/controllers/matchController.js
+++ server/controllers/matchController.js
@@ -5,13 +5,13 @@ module.exports = {
 
 	createMatch: (username) => {
   	db.cypher({
-  	    query: 'MATCH (user:Person)-[r:INTERESTED_IN]->(subreddit)<-[:INTERESTED_IN]-(potential:Person) WHERE user.name = {username} MERGE (user)<-[:POTENTIAL]->(potential) RETURN user, potential, r;',
+  	    query: 'MATCH (user:Person)-[r:FOLLOWS]->(subreddit)<-[:FOLLOWS]-(potential:Person) WHERE user.name = {username} MERGE (user)<-[:POTENTIAL]->(potential) RETURN user, potential, r;',
   	    params: {
   	    	username: username,
   	    }
   	}, function (err, results) {
   		  if (err) {
-  		    console.log("issue with ": err)
+  		    console.log("issue with: ", err)
   		  } else {
   		    console.log('list of potentials', results);
   		  }
