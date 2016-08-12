e16f91df2671036a66edb229c3c9d33b8ea13857
Christine Chou <christinejchou@gmail.com>
2016-08-12 10:41:09 -0700

    Re-add reddit key locations and change neo4j pw

--- server/db/config.js
+++ server/db/config.js
@@ -5,7 +5,7 @@ const model = require('seraph-model');
 const db = seraph({
   server: 'http://localhost:7474',
   user: 'neo4j',
-  pass: 'neo4j',
+  pass: 'cake',
 });
 const Person = model(db, 'person');
 const Subreddit = model(db, 'subreddit');
