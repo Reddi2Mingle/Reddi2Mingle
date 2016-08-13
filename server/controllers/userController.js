a357dc58cb71bda2b497d58f0dbe1e2bded7f7aa
Tyler Becks <tylerdbecks@gmail.com>
2016-08-13 11:25:31 -0700

    Change dummy data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,10 @@
 const request = require('request');
-const REDDIT_CONSUMER_KEY = process.env.REDDIT_KEY;
-const REDDIT_CONSUMER_SECRET = process.env.REDDIT_SECRET;
 const bluebird = require('bluebird');
 const potentialController = require('./potentialController');
 const neo4j = require('neo4j');
+
+const REDDIT_CONSUMER_KEY = process.env.REDDIT_KEY;
+const REDDIT_CONSUMER_SECRET = process.env.REDDIT_SECRET;
 const db = new neo4j.GraphDatabase('http://neo4j:cake@localhost:7474');
 
 const dummyData = {
@@ -36,28 +37,28 @@ const dummyData = {
 };
 
 const matches = [
-    {
-      redditId: 'rshiei4n4',
-      name: 'Casper Holmgreen',
-      photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAhtAAAAJDNkM2M5YjdmLWYxZTUtNDg1MS04N2EwLTYyYjlmYTYxYzY1ZQ.jpg',
-      subreddits: ['Office Depot', 'Dog Mania', 'Math all over me'],
-      messageUrl: 'https://www.linkedin.com/msgToConns?displayCreate=&connId=127002602&goback=%2Enpv_AAkAAAeR5*5oBj6QjZ0FlpDYyszbSd1d*4GTkKsik_*1_*1_NAME*4SEARCH_D3Ov_*1_en*4US_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_tyah_*1_*1&trk=prof-0-sb-message-button',
-    },
-    {
-      redditId: 'rsie34',
-      name: 'Jeremy Toce',
-      photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/07d/3ed/24801ee.jpg',
-      subreddits: ['Lydia', 'LeatherDaddyLand', 'Growth Spurts'],
-      messageUrl: 'https://www.linkedin.com/msgToConns?displayCreate=&connId=70699564&goback=%2Enpv_AAkAAAQ2yiwBGi9YkWLvYwSkfG*5uihI7x1J8jyk_*1_*1_NAME*4SEARCH_OhvK_*1_en*4US_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_tyah_*1_*1&trk=prof-0-sb-message-button',
-    },
-    {
-      redditId: '12en2e32e',
-      name: 'Sunny Virk',
-      photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAMAAAAAJGVlNzYyYzMxLTllYmUtNDFlNy04ZTVmLWEzZmEyNzZjN2Q1MA.jpg',
-      subreddits: ['curry kitchen', 'webpack nerds', 'pedicures'],
-      messageUrl: 'https://www.linkedin.com/msgToConns?displayCreate=&connId=401581374&goback=%2Enpv_AAkAABfvpT4BAJPbMq0DeYUI7iwpuHAAZqlWYwY_*1_*1_NAME*4SEARCH_aJ8d_*1_en*4US_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_tyah_*1_*1&trk=prof-0-sb-message-button',
-    },
-  ];
+  {
+    redditId: 'rshiei4n4',
+    name: 'Casper Holmgreen',
+    photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAhtAAAAJDNkM2M5YjdmLWYxZTUtNDg1MS04N2EwLTYyYjlmYTYxYzY1ZQ.jpg',
+    subreddits: ['Office Depot', 'Dog Mania', 'Math all over me'],
+    messageUrl: 'https://www.linkedin.com/msgToConns?displayCreate=&connId=127002602&goback=%2Enpv_AAkAAAeR5*5oBj6QjZ0FlpDYyszbSd1d*4GTkKsik_*1_*1_NAME*4SEARCH_D3Ov_*1_en*4US_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_tyah_*1_*1&trk=prof-0-sb-message-button',
+  },
+  {
+    redditId: 'rsie34',
+    name: 'Jeremy Toce',
+    photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/07d/3ed/24801ee.jpg',
+    subreddits: ['Lydia', 'LeatherDaddyLand', 'Growth Spurts'],
+    messageUrl: 'https://www.linkedin.com/msgToConns?displayCreate=&connId=70699564&goback=%2Enpv_AAkAAAQ2yiwBGi9YkWLvYwSkfG*5uihI7x1J8jyk_*1_*1_NAME*4SEARCH_OhvK_*1_en*4US_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_tyah_*1_*1&trk=prof-0-sb-message-button',
+  },
+  {
+    redditId: '12en2e32e',
+    name: 'Sunny Virk',
+    photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAMAAAAAJGVlNzYyYzMxLTllYmUtNDFlNy04ZTVmLWEzZmEyNzZjN2Q1MA.jpg',
+    subreddits: ['curry kitchen', 'webpack nerds', 'pedicures'],
+    messageUrl: 'https://www.linkedin.com/msgToConns?displayCreate=&connId=401581374&goback=%2Enpv_AAkAABfvpT4BAJPbMq0DeYUI7iwpuHAAZqlWYwY_*1_*1_NAME*4SEARCH_aJ8d_*1_en*4US_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_*1_tyah_*1_*1&trk=prof-0-sb-message-button',
+  },
+];
 
 
 // Request list of user's subscribed subreddits
@@ -67,20 +68,20 @@ queryUserSubreddits = (redditId) => {
       query: 'MATCH (user:Person)-[r:FOLLOWS]->(subreddit) WHERE user.redditId={redditId} RETURN subreddit;',
       params: {
         redditId: redditId,
-      }
+      },
     }, function (err, subreddits) {
       if (err) {
         console.log('error');
-        reject(err)
+        reject(err);
       } else {
-        var subredditList = subreddits.map(function(item) {
+        var subredditList = subreddits.map(function (item) {
           return item.subreddit.properties.name;
         });
         resolve(subredditList);
       }
-    })
-  })
-},
+    });
+  });
+};
 
 // Get the user's temporary access token
 queryAccessToken = (redditId) => {
@@ -89,14 +90,14 @@ queryAccessToken = (redditId) => {
       query: 'MATCH (n:Person) WHERE n.redditId={redditId} return n.accessToken;',
       params: {
         redditId: redditId,
-      }
+      },
     }, function (err, results) {
       if (err) {
         console.log('issue with retrieving', err);
         reject(err);
       } else {
-        console.log('here is the accessToken', results[0]['n.accessToken']);
-        resolve(results[0]['n.accessToken'])
+        console.log('here is the accessToken', results[0]);
+        resolve(results[0]);
       }
     })
   })
@@ -171,9 +172,8 @@ createUserSubreddits = (redditId) => {
 },
 
 module.exports = {
-
   sendDummyData: (req, res) => {
-    res.send(dummyData)
+    res.send(dummyData);
   },
 
   createNewUser: (profile, accessToken, refreshToken) => {
