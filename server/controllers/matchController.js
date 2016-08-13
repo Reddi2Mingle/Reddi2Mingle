7ab2e3e21ac9e3e2aff8fc21c7d9fa4b9add3f8b
Christine Chou <christinejchou@gmail.com>
2016-08-13 13:10:56 -0700

    Implement send matches

--- server/controllers/matchController.js
+++ server/controllers/matchController.js
@@ -2,23 +2,56 @@ const neo4j = require('neo4j');
 const db = new neo4j.GraphDatabase('http://neo4j:cake@localhost:7474');
 
 module.exports = {
+  //returns array of matches for user
+	showMatches: (req, res) => {
+    // params: redditId, potentialId, swipe (need from client)
+    let r = req.query;
+    let user = r.redditId;
+    // console.log('r',r,'user',user)
 
-	createMatch: (username) => {
-  	db.cypher({
-  	    query: 'MATCH (user:Person)-[r:FOLLOWS]->(subreddit)<-[:FOLLOWS]-(potential:Person) WHERE user.name = {username} MERGE (user)<-[:POTENTIAL]->(potential) RETURN user, potential, r;',
-  	    params: {
-  	    	username: username,
-  	    }
-  	}, function (err, results) {
-  		  if (err) {
-  		    console.log("issue with: ", err)
-  		  } else {
-  		    console.log('list of potentials', results);
-  		  }
-  	});
-  },
-  showMatches: () => {
-    
+    db.cypher({
+      query: `MATCH (user:Person {redditId: ${user}})-[r:MATCH]-(matched:Person) RETURN matched`
+    }, function(err, matched) {
+      if (err) {
+        console.log('Error in finding potential interest for user:',err);
+      } else {
+        matched = matched.reduce(function(prev, cur) {
+          var person = cur.matched.properties;
+          prev[person.name] = person;
+          return prev;
+        },{})
+        console.log('matched',matched)
+        res.send(matched)
+        }
+    });
+    // Example response:
+    // [
+    //   {
+    //     "matched": {
+    //       "_id": 16,
+    //       "labels": [
+    //         "Person"
+    //       ],
+    //       "properties": {
+    //         "name": "Sompop Corn",
+    //         "photo": "https://cdn1.iconfinder.com/data/icons/simple-icons/4096/reddit-4096-black.png",
+    //         "redditId": "6"
+    //       }
+    //     }
+    //   },
+    //   {
+    //     "matched": {
+    //       "_id": 15,
+    //       "labels": [
+    //         "Person"
+    //       ],
+    //       "properties": {
+    //         "name": "Trevor Healy",
+    //         "photo": "https://cdn1.iconfinder.com/data/icons/simple-icons/4096/reddit-4096-black.png",
+    //         "redditId": "5"
+    //       }
+    //     }
+    //   }
+    // ]
   }
-
 }
--- server/helpers/routes.js
+++ server/helpers/routes.js
@@ -5,18 +5,19 @@ const authController = require('../controllers/authController.js');
 const userController = require('../controllers/userController.js');
 const potentialController = require('../controllers/potentialController.js');
 const swipeController = require('../controllers/swipeController.js');
+const matchController = require('../controllers/matchController.js');
 
 module.exports = (socket, io, app) => {
 
   app.get('/auth/reddit', authController.crypto);
   app.get('/auth/reddit/callback', passport.authenticate('reddit', { failureRedirect: '/signup'}), authController.login);
+  app.get('/dummyData', userController.sendDummyData);
   app.get('/userInfo', userController.queryUserInfo);
   // app.get('/subreddits', userController.createUserSubreddits);
   app.get('/potentials', potentialController.queryPotentials);
   app.get('/createPotentials', potentialController.createPotentials);
   app.post('/swipe', swipeController.likeResponse);
-  app.get('/dummyData', userController.sendDummyData);
-  // app.get('/match', swipe.showMatches)
+  app.get('/match', matchController.showMatches)
 
 	// send all requests to index.html so browserHistory in React Router works
   app.get('*', (req, res) => {
