cf43fe5516286b7eb318ac4fe106c31a2069bac9
Jennifer Ong <jenn.r.ong@gmail.com>
2016-08-13 10:32:28 -0700

    Finish styling heart icon in potential

--- client/app/potential/Potential.jsx
+++ client/app/potential/Potential.jsx
@@ -24,27 +24,27 @@ export default class Potential extends Component {
           <h2>Loading...</h2>
         </div>
            :
-        <div className="potential-view"> 
-            <div className="potential-card">
-              <img src={picUrl} className="full-profile-image" />
-              <div className="potential-info">
-                <h3>{username}</h3>
-                <div className="potential-more-info">
-                  <i className="material-icons md-48 orange">favorite</i>
-                  <span> r/ </span>
-                  <div className="subreddit-list">
-                    <ul>
-                      {subreddits.map((subreddit) => (
-                        <span>{subreddit}</span>
-                      ))}
-                    </ul>
+        <div className="potential-view">
+          <div className="potential-card">
+            <img src={picUrl} className="full-profile-image" />
+            <div className="potential-info">
+              <h3>{username}</h3>
+              <div className="potential-more-info">
+                <i className="material-icons md-48 orange">favorite</i>
+                <span className="heart-text"> r/ </span>
+                <div className="subreddit-list">
+                  <ul>
+                    {subreddits.map((subreddit) => (
+                      <span>{subreddit}</span>
+                    ))}
+                  </ul>
                 </div>
               </div>
             </div>
             <div className="like-dislike-group">
-              <button> <i className="material-icons md-48 md-light"> sentiment_dissatisfied </i>
+              <button> <i className="material-icons md-48 white"> sentiment_dissatisfied </i>
               </button>
-              <button> <i className="material-icons md-48 md-light"> sentiment_satisfied </i>
+              <button> <i className="material-icons md-48 white"> sentiment_satisfied </i>
               </button>
             </div>
           </div>
