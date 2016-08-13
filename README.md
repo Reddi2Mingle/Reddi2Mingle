5e527f4a4213429f2d381c294968f3ee6ffd4331
Christine Chou <christinejchou@gmail.com>
2016-08-13 14:45:46 -0700

    Complete matches and add input/output examples to README

--- README.md
+++ README.md
@@ -31,26 +31,41 @@ Output:
 Action: 
 Input:
 Output:
-6. Route: app.get('/potentials', potentialController.queryPotentials);
+6. Route: app.get('/createPotentials', potentialController.createPotentials);
+
+
+7. Route: app.get('/potentials', potentialController.queryPotentials);
 Action: 
 Input:
 Output:
-7. Route: app.post('/swipe', swipeController.likeResponse);
+8. Route: app.post('/swipe', swipeController.likeResponse);
 Action: 'POST' 
 Input: redditId, potentialId, swipe
 Output: 
  - Relationship created (INTEREST || MATCH || NEVER)
-[
-  {
-    "_id": 36,
-    "type": "NEVER",
-    "properties": {},
-    "_fromId": 18,
-    "_toId": 15
+  [
+    {
+      "_id": 36,
+      "type": "NEVER",
+      "properties": {},
+      "_fromId": 18,
+      "_toId": 15
+      }
+  ]
+
+9. Route: app.get('/match', matchController.showMatches)
+Action:
+Input:
+Output:
+  [
+    {
+      "name": "Jay Arella",
+      "photo": "https://cdn1.iconfinder.com/data/icons/simple-icons/4096/reddit-4096-black.png",
+      "redditId": "7"
+    },
+    {
+      "name": "Shea Hawkins",
+      "photo": "https://cdn1.iconfinder.com/data/icons/simple-icons/4096/reddit-4096-black.png",
+      "redditId": "9"
     }
-]
-
-
-app.get('/createPotentials', potentialController.createPotentials);
-
-app.get('/match', matchController.showMatches)
+  ]
--- server/controllers/matchController.js
+++ server/controllers/matchController.js
@@ -4,40 +4,19 @@ const db = new neo4j.GraphDatabase('http://neo4j:cake@localhost:7474');
 module.exports = {
   //returns array of matches for user
 	showMatches: (req, res) => {
-    // params: redditId, potentialId, swipe (need from client)
-    let r = req.query;
-    let user = r.redditId;
-    // console.log('r',r,'user',user)
+	// params: redditId
+	let r = req.query;
+	let user = r.redditId;
 
-    db.cypher({
-      query: `MATCH (user:Person {redditId: ${user}})-[r:MATCH]-(matched:Person) RETURN matched`
-    }, function(err, matched) {
-      if (err) {
-        console.log('Error in finding potential interest for user:',err);
-      } else {
-        matched = matched.reduce(function(prev, cur) {
-          var person = cur.matched.properties;
-          prev[person.name] = person;
-          return prev;
-        },{})
-        console.log('matched',matched)
-        res.send(matched)
-        }
-    });
-    // Example response:
-    // [
-    //    {
-    //       "name": "Sompop Corn",
-    //       "photo": "https://cdn1.iconfinder.com/data/icons/simple-icons/4096/reddit-4096-black.png",
-    //       "redditId": "6"
-    //     }
-    //   },
-    //   {
-    //      "name": "Trevor Healy",
-    //      "photo": "https://cdn1.iconfinder.com/data/icons/simple-icons/4096/reddit-4096-black.png",
-    //      "redditId": "5"
-    //     }
-    //   }
-    // ]
+	db.cypher({
+	  query: `MATCH (user:Person {redditId: ${user}})-[r:MATCH]-(matched:Person) RETURN matched`
+	}, function(err, matched) {
+	  if (err) {
+		console.log('Error in finding potential interest for user:',err);
+	  } else {
+			var matched = matched.map(function(v){return v.matched.properties})
+			res.send(matched)
+		}
+	});
   }
 }
