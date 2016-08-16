dbe0cca81ee57a8828b22d85bfe9ba984624a834
Jennifer Ong <jenn.r.ong@gmail.com>
2016-08-16 12:32:03 -0700

    Add mustache and lips images to container

--- client/app/user/preferences/Preferences.jsx
+++ client/app/user/preferences/Preferences.jsx
@@ -4,8 +4,24 @@ import React, { Component } from 'react';
 export default class Preferences extends Component {
   render() {
     return (
-      <div>
-        <h1> Hello World </h1>
+      <div className="preferences-view">
+        <h1> Basics First! </h1>
+        <div className="preferences-container">
+          <h2>
+            I am a
+          </h2>
+          <img
+            src="../../../assets/img/mustache.png"
+            alt="mustache"
+          />
+          <img
+            src="../../../assets/img/lips.png"
+            alt="lips"
+          />
+          <h2>
+            Looking for a 
+          </h2>
+        </div>
       </div>
     );
   }
