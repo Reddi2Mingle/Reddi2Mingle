610c3f7234bf5577d1c5775ded4f109079804185
Jennifer Ong <jenn.r.ong@gmail.com>
2016-08-16 15:26:40 -0700

    Update image links for preferences

--- client/app/user/preferences/Preferences.jsx
+++ client/app/user/preferences/Preferences.jsx
@@ -5,22 +5,42 @@ export default class Preferences extends Component {
   render() {
     return (
       <div className="preferences-view">
-        <h1> Basics First! </h1>
+        <h1> Basics First </h1>
         <div className="preferences-container">
           <h2>
             I am a
           </h2>
-          <img
-            src="../../../assets/img/mustache.png"
-            alt="mustache"
-          />
-          <img
-            src="../../../assets/img/lips.png"
-            alt="lips"
-          />
+          <div className="preferences-options">
+            <a>
+              <img
+                src="../../../assets/img/mustache.png"
+                alt="mustache"
+              />
+            </a>
+            <span>
+              <img
+                src="../../../assets/img/lips.png"
+                alt="lips"
+              />
+            </span>
+          </div>
           <h2>
-            Looking for a 
+            Looking for a
           </h2>
+          <div className="preferences-options">
+            <span>
+              <img
+                src="../../../assets/img/mustache.png"
+                alt="mustache"
+              />
+            </span>
+            <span>
+              <img
+                src="../../../assets/img/lips.png"
+                alt="lips"
+              />
+            </span>
+          </div>
         </div>
       </div>
     );
