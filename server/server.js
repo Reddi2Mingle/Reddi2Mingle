d2eac6ad78bcfbc33786d571b122255f710304da
Tyler Becks <tylerdbecks@gmail.com>
2016-08-19 20:33:59 -0700

    Move sockets server.js and delete env.sh

--- server/server.js
+++ server/server.js
@@ -5,11 +5,30 @@ const middleware = require('./helpers/middleware');
 const routers = require('./helpers/routes');
 require('./auth/passport');
 require('./db/neo4jconfig');
-// require('./helpers/seed');
-// require('./helpers/seedCreation');
 
 middleware(app);
 routers(app);
+
+var users = {};
+io.on('connection', (socket) => {
+  console.log('sockets connected!');
+  socket.on('save my id', (redditId) => {
+    users[redditId] = socket.id;
+    console.log(`Id saved! Your socket.id: ${socket.id}, and your redditId: ${redditId}`);
+  });
+
+  // Sending message to Specific user
+  socket.on('send msg', (senderId, receiverId) => {
+    if (users[receiverId]) {
+      socket.broadcast.to(users[receiverId]).emit('get msg', `You got pinged by ${senderId}`);
+    }
+  });
+
+  // Remove user from our storage object when they disconnect
+  socket.on('disconnect', (redditId) => {
+    delete users[redditId];
+  });
+});
 
 // App now listening on port 80
 server.listen(process.env.PORT_APP, (err) => {
