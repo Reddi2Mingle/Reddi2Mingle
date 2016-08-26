fb8bb2da51ea4ca5a61030efe29f4aca02e3f379
Tyler Becks <tylerdbecks@gmail.com>
2016-08-26 12:22:07 -0700

    Add one more test

--- test/apiTests.js
+++ test/apiTests.js
@@ -21,9 +21,9 @@ describe('Reddi2Mingle', () => {
 
   describe('api', () => {
     describe('/api/userInfo', () => {
-      it('responds with a 200 (OK)', done => {
+      it('sends back necessary info about user', done => {
         request(app)
-          .get('/api/userInfo/')
+          .get('/api/userInfo')
           .query({ redditId: 'j8636' })
           .end((err, res) => {
             if (err) {
@@ -35,11 +35,50 @@ describe('Reddi2Mingle', () => {
             expect(retrieved.photo).to.be.a('string');
             expect(retrieved.gender).to.be.a('string');
             expect(retrieved.preference).to.be.a('string');
+            expect(retrieved.trophyCount).to.be.a('number');
+            expect(retrieved.postKarma).to.be.a('number');
+            expect(retrieved.commentKarma).to.be.a('number');
+            expect(retrieved.receivedUpvotes).to.be.a('number');
+            expect(retrieved.receivedDownvotes).to.be.a('number');
+            expect(retrieved.deliveredUpvotes).to.be.a('number');
+            expect(retrieved.deliveredDownvotes).to.be.a('number');
             expect(retrieved.subreddits).to.be.a('array');
+            done();
+          });
+      });
+      it('excludes password, accessToken, and refreshToken', done => {
+        request(app)
+          .get('/api/userInfo')
+          .query({ redditId: 'j8636' })
+          .end((err, res) => {
+            if (err) {
+              return done(err);
+            }
+            var retrieved = JSON.parse(res.body.body);
+            expect(retrieved.password).to.equal(undefined);
+            expect(retrieved.accessToken).to.equal(undefined);
+            expect(retrieved.refreshToken).to.equal(undefined);
             done();
           });
       });
     });
 
+    describe('/api/userInfo/updatePassword', () => {
+      it('sends back a 200 (OK)', done => {
+        request(app)
+          .get('/api/userInfo/updatePassword')
+          .query({ redditId: 'j8636' })
+          .end((err, res) => {
+            if (err) {
+              return done(err);
+            }
+            var retrieved = JSON.parse(res.body.body);
+            expect(retrieved.password).to.equal(undefined);
+            expect(retrieved.accessToken).to.equal(undefined);
+            expect(retrieved.refreshToken).to.equal(undefined);
+            done();
+          });
+      });
+    });
   });
 });
