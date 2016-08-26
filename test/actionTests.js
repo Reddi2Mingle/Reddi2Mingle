fcdf1bb44547d19b15cb536e6a32b01ab175e456
Tyler Becks <tylerdbecks@gmail.com>
2016-08-25 20:00:33 -0700

    Extract out port listener to index.js

--- test/actionTests.js
+++ test/actionTests.js
@@ -1,22 +1,22 @@
-import expect from 'expect';
-import { addSubreddit, updateUsername } from '../client/app/potential/PotentialActions';
+// import expect from 'expect';
+// import { addSubreddit, updateUsername } from '../client/app/potential/PotentialActions';
 
 
-describe('Default Test', () => {
-  it('Hooray, you passed a test! You are now rocking all the coffee power', () => {
-    expect(true).toBe(true);
-  });
-});
+// describe('Default Test', () => {
+//   it('Hooray, you passed a test! You are now rocking all the coffee power', () => {
+//     expect(true).toBe(true);
+//   });
+// });
 
-describe('Client Components', () => {
-  describe('Potential Components', () => {
-    it ('should change username of potential person', () => {
-      const payload = 'John Smith';
-      const expectedAction = {
-        type: 'UPDATE_USERNAME',
-        payload: payload,
-      };
-      expect(updateUsername(payload)).toEqual(expectedAction);
-    });
-  });
-});
+// describe('Client Components', () => {
+//   describe('Potential Components', () => {
+//     it ('should change username of potential person', () => {
+//       const payload = 'John Smith';
+//       const expectedAction = {
+//         type: 'UPDATE_USERNAME',
+//         payload: payload,
+//       };
+//       expect(updateUsername(payload)).toEqual(expectedAction);
+//     });
+//   });
+// });
---
+++ test/apiTests.js
@@ -0,0 +1,51 @@
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+
+// const server = require('../server/server');
+
+// const should = chai.should();
+// chai.use(chaiHttp);
+
+
+// describe('Potentials', () => {
+//   describe('GET /api/potentials', done => {
+//     chai.request(server)
+//       .get('/api/potentials')
+//       .query({ redditId: 'j8636' })
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         res.should.be.a('array');
+//         res.body.length.should.be.eql(0);
+//         done();
+//       });
+//   });
+// });
+const chai = require('chai');
+const request = require('supertest');
+const server = require('../server/server.js');
+const expect = chai.expect;
+const nock = require('nock');
+
+describe('Reddi2Mingle', () => {
+  var app;
+  beforeEach(done => {
+    app = server.listen(3000, (err) => {
+      err ? console.log(`Error connecting the server: ${err}`) :
+      console.log(`Server listening on ${process.env.PORT_APP}`);
+      done();
+    });
+  });
+  afterEach(() => {
+    app.close();
+  });
+
+  describe('api', () => {
+    describe('/api/userInfo', () => {
+      it('responds with a 200 (OK)', done => {
+        request(server)
+          .get('/api/userInfo')
+          .expect(200, done);
+      });
+    });
+  });
+});
