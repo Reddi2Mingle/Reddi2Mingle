034bf44dd96cfb722275be10a0be831f9d067d37
Jennifer Ong <jenn.r.ong@gmail.com>
2016-08-31 16:30:05 -0700

    Delete seed files

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const middleware = require('./helpers/middleware');
 const routers = require('./helpers/routes');
 require('./auth/passport');
 require('./db/neo4jconfig');
-// require('./helpers/seedCreation');
 
 middleware(app);
 routers(app);
