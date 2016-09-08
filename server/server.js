376763f61d1ee854d8cb5eee5d94217208eaa2ec
Tyler Becks <tylerdbecks@gmail.com>
2016-09-08 15:21:10 -0700

    Clean up socket console.logs

--- server/server.js
+++ server/server.js
@@ -20,11 +20,9 @@ client.on('error', err => {
 });
 
 io.on('connection', socket => {
-  console.log('SOCKETS --- sockets connected!');
   socket.on('save my id', redditId => {
     client.set(redditId, socket.id, redis.print);
     client.keys('*', (err, replies) => {
-      console.log(`cached redditIds ${replies}`);
       client.mget(replies, redis.print);
     });
   });
@@ -34,13 +32,12 @@ io.on('connection', socket => {
     const receiverId = payload.receiverId;
     const userInfo = payload.userInfo;
     if (client.get(receiverId)) {
-      console.log('emitting userInfo');
       socket.broadcast.to(client.get(receiverId)).emit('get new match', userInfo);
     }
   });
 
   socket.on('disconnect', disconnection => {
-    console.log(`diconnecting via ${disconnection}`);
+    console.log(`diconnecting sockets`);
   });
 });
 
